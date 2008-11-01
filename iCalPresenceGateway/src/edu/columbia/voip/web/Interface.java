ba6e2951eae6e07e89579008d302ae0537eea732
morales.john <morales.john@958b86a0-a85d-11dd-9c66-c9f8973bfffa>
2008-11-01 22:06:40 +0000

    * adding placeholder for web interface. Not sure how this is going to work yet-- may do something different later.

---
+++ iCalPresenceGateway/src/edu/columbia/voip/web/Interface.java
@@ -0,0 +1,22 @@
+/*
+ * Interface.java
+ *
+ * Created on November 1, 2008, 7:05 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package edu.columbia.voip.web;
+
+/**
+ *
+ * @author jmoral
+ */
+public class Interface {
+    
+    /** Creates a new instance of Interface */
+    public Interface() {
+    }
+    
+}
