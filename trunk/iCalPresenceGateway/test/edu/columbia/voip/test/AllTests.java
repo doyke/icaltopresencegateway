2e91157659969dfe58c6e283f4779af257a7d9c7
morales.john <morales.john@958b86a0-a85d-11dd-9c66-c9f8973bfffa>
2008-12-05 17:52:29 +0000

    * move dumpCalendar debug method to static method in AllTests

--- trunk/iCalPresenceGateway/test/edu/columbia/voip/test/AllTests.java
+++ trunk/iCalPresenceGateway/test/edu/columbia/voip/test/AllTests.java
@@ -1,5 +1,11 @@
 package edu.columbia.voip.test;
 
+import java.util.Iterator;
+
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Component;
+import net.fortuna.ical4j.model.Date;
+import net.fortuna.ical4j.model.Property;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
@@ -17,6 +23,20 @@ public class AllTests {
 		//$JUnit-END$
 		
 		return suite;
+	}
+	
+	public static void dumpCalendar(Calendar calendar)
+	{
+		System.out.println("\n=-=- DUMPING CALENDAR FIELDS =-=-");
+		for (Iterator i = calendar.getComponents().iterator(); i.hasNext();) {
+            Component component = (Component) i.next();
+            System.out.println("Component [" + component.getName() + "]");
+
+            for (Iterator j = component.getProperties().iterator(); j.hasNext();) {
+                Property property = (Property) j.next();
+                System.out.println("Property [" + property.getName() + ", " + property.getValue() + "]");
+            }
+        }
 	}
 
 }
