2ef339ec879dc21f8505b3ccf99f1750d083ed01
morales.john <morales.john@958b86a0-a85d-11dd-9c66-c9f8973bfffa>
2008-12-14 03:51:15 +0000

    * now have option for thread pooling or simply straight theading * presence messaging working
    /// TODO
    * calendar parsing still needs to handle 
      a.) events with no modified date, and 
      b.) events with no end date, need to user duration.

--- iCalPresenceGateway/src/edu/columbia/voip/server/RegistrationDispatch.java
+++ iCalPresenceGateway/src/edu/columbia/voip/server/RegistrationDispatch.java
@@ -9,6 +9,7 @@
 
 package edu.columbia.voip.server;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -17,6 +18,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import edu.columbia.voip.server.conf.ServerParameters;
+import edu.columbia.voip.user.CalendarAccount;
 import edu.columbia.voip.user.GatewayUser;
 
 /**
@@ -40,21 +42,47 @@ public class RegistrationDispatch extends Thread {
     public void run()
     {
         // TODO: handle client connection here
-    	_logger.log(Level.INFO, "Got registration connection from: " + _clientSocket.getInetAddress().getHostAddress());
-    	_logger.log(Level.INFO, "Begin reading stream for serialized GatewayUser");
+    	_logger.log(Level.INFO, "Got registration connection from: " + _clientSocket.getInetAddress().getHostAddress() + ", reading stream.");
     	GatewayUser newUser = null;
     	
-        // TODO: Read inputsteam from servlet to get GatewayUser
+    	String key = null;
+    	String userid = null;
+    	String pass = null;
+    	String host = null;
+    	String uri = null;
+    	String port = null;
+    	String ssl = null;
+    	
+        // TODO: Read input stream from servlet to get GatewayUser
         try { 
-        	ObjectInputStream ois = new ObjectInputStream(_clientSocket.getInputStream());
-        	newUser = (GatewayUser)ois.readObject();
+        	//ObjectInputStream ois = new ObjectInputStream(_clientSocket.getInputStream());
+        	DataInputStream ois = new DataInputStream(_clientSocket.getInputStream());
+        	
+        	key = ois.readUTF();
+        	userid = ois.readUTF();
+        	pass = ois.readUTF();
+        	host = ois.readUTF();
+        	uri = ois.readUTF();
+        	port = ois.readUTF();
+        	ssl = ois.readUTF();
+        	
+        	CalendarAccount calAccount = new CalendarAccount(	userid, 
+        														pass.toCharArray(), 
+    															host, 
+																uri, 
+																Integer.parseInt(port), 
+																Boolean.parseBoolean(ssl));
+        	newUser = GatewayUser.createUser(key, calAccount, null);
+        	//newUser = (GatewayUser)ois.readObject();
         	ois.close();
     	}
         catch (IOException e)	{ _logger.log(Level.SEVERE, "caught IOException in registration dispatch", e); }
-        catch (ClassNotFoundException e) { _logger.log(Level.SEVERE, "caught ClassNotFoundException in registration dispatch.", e); }
         
         if (newUser != null)
+        {
+        	_logger.log(Level.INFO, "Hot-loading new registration of user " + newUser.getPrimaryKey() + " to GatewayThread");
         	_gatewayThread.addGatewayUser(newUser);
+        }
         else
         	_logger.log(Level.SEVERE, "user received from registration servlet is NULL, not adding to gateway thread.");
     }
