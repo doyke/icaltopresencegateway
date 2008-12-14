eac9ed0fce88eb5b1169eda368122ec40096317c
milind.nimesh <milind.nimesh@958b86a0-a85d-11dd-9c66-c9f8973bfffa>
2008-12-14 00:30:38 +0000

    Includes Presence msg updates

--- trunk/iCalPresenceGateway/src/edu/columbia/voip/presence/Presence.java
+++ trunk/iCalPresenceGateway/src/edu/columbia/voip/presence/Presence.java
@@ -6,37 +6,59 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package edu.columbia.voip.presence;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import edu.columbia.voip.user.GatewayUser;
-
+import javax.sip.InvalidArgumentException;
+import javax.sip.SipException;
 
 /**
  *
  * @author jmoral
  */
 public class Presence {
-	
-	private static SipLayer _sipLayer = null;
-    
+
+    private static SipLayer _sipLayer = null;
+
     /** Cannot be instantiated for now-- only have the one static sendMessage method */
     private Presence() {
     }
 
-	public static void sendMessage(String userid, String summary, String description,
-			String location, String category, Date start, Date end)
-	{
-		Logger.getLogger(Presence.class.getName()).log(Level.INFO, "Got request to send SIP presence message to calendar event");
-		// TODO @Milind, send this calendar event to the presence server for user <user>
-	}
+    public static void sendMessage(String userid, String summary, String description,
+            String location, String category, Date start, Date end) {
+        Logger.getLogger(Presence.class.getName()).log(Level.INFO, "Got request to send SIP presence message to calendar event");
+        // TODO @Milind, send this calendar event to the presence server for user <user>
+        String pidfMsg = createPidf(userid, location, summary, description, start.toString(), end.toString(), category);
+        try {
+            
+            _sipLayer.sendMessage(userid, "sip:presence@128.59.18.182:5060", pidfMsg);
+            
+        } catch (ParseException ex) {
+            Logger.getLogger(Presence.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidArgumentException ex) {
+            Logger.getLogger(Presence.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SipException ex) {
+            Logger.getLogger(Presence.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-	public static void setSipLayer(SipLayer _sipLayer)
-	{
-		Presence._sipLayer = _sipLayer;
-	}
+
+
+
+    }
+
+    public static void setSipLayer(SipLayer _sipLayer) {
+        Presence._sipLayer = _sipLayer;
+    }
+
+    private static String createPidf(String userid, String location, String summary, String description, String startDate, String endDate, String category) {
+        
+        String pidf = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><presence xmlns=\"urn:ietf:params:xml:ns:pidf\" xmlns:local=\"urn:example-com:pidf-status-type\" entity=\"pres:"+userid+"@columbia.edu\"><tuple id=\"ub93s3\"><status><basic>closed</basic><local:location>"+location+"</local:location></status></tuple><note>"+summary+";"+description+";"+startDate+";"+endDate+";"+category+"</note>></presence>";
+        
+        return pidf;
+    }
 }
