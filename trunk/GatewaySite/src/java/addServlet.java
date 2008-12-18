32d2a11b39728b925b398c5fc0007d9c735e39ea
morales.john <morales.john@958b86a0-a85d-11dd-9c66-c9f8973bfffa>
2008-12-18 23:30:21 +0000

    * added some error handling and error page to the gateway site for: == problems connecting to either the presence or registration DB. == problem connecting to gateway thread.

--- trunk/GatewaySite/src/java/addServlet.java
+++ trunk/GatewaySite/src/java/addServlet.java
@@ -13,38 +13,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author milind
  */
-public class addServlet extends HttpServlet {
-
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     * @param request servlet request
-     * @param response servlet response
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        System.out.println("check3");
-        try {
-            /* TODO output your page here
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet addServlet</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet addServlet at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-             */
-        } finally {
-            out.close();
-        }
-    }
+public class addServlet extends HttpServlet 
+{
+    private static final int CODE_OK = -1;
+    private static final int CODE_DB = 0;
+    private static final int CODE_GATEWAY_THREAD = 1;
+    private static final int CODE_TOMCAT = 2;
+    
+    private static final String[] ERROR_MESSAGES = { "Could not connect to either the registration or presence DB. Are both of the DBs running? Does the servlet have the correct network address / credentials?",
+                                                     "Could not connect to gateway thread. Is there iCal to Presence gateway running?",
+                                                     "There was a Java container error. Is your web server and container configured correctly?",
+                                                     };
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
@@ -57,7 +42,7 @@ public class addServlet extends HttpServlet {
 
         PrintWriter out = response.getWriter();
         String enteredUni = request.getParameter("enteredUni");
-
+        
         Connection conn = null;
         int returnCode = -1;
         try {
@@ -107,11 +92,10 @@ public class addServlet extends HttpServlet {
      * @param response servlet response
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-
+            throws ServletException
+    {
         //Extract Data
-
+        int ret = CODE_OK;
         String uni = request.getParameter("uni");
         String icalid = request.getParameter("icalid");
         String icalpass = request.getParameter("icalpass");
@@ -119,41 +103,60 @@ public class addServlet extends HttpServlet {
         String icaluri = request.getParameter("icaluri");
         String icalport = request.getParameter("icalport");
         String ssl = request.getParameter("ssl");
-        boolean sslVal = false;
-
-        if (ssl.equals("Yes")) {
-            sslVal = true;
-        } else {
-            sslVal = false;
+        boolean sslVal = ssl.equals("Yes");
+        
+        String userName = "btuser";
+        String passwd = "passwd";
+        String urlRegistration = "jdbc:mysql://localhost:3306/icalgateway";
+        String urlPresence = "jdbc:mysql://128.59.18.182:3306/Presence";
+        
+        Connection connRegistration = null;
+        Connection connPresence = null;
+        try {
+            // make sure we can connect to both databases before doing the INSERTs
+            connRegistration = createDBConn(userName, passwd, urlRegistration);
+            connPresence = createDBConn(userName, passwd, urlPresence);
+            
+            addUserRegistrationDB(connRegistration, uni, icalid, icalpass, icalurl, icaluri, icalport, ssl);
+            addUserToPresenceDB(connPresence, uni, icalpass);
+            pipeRegistrationToGateway(uni, icalid, icalpass, icalurl, icaluri, icalport, ssl);
+        } 
+        catch (IllegalAccessException e)    { ret = CODE_TOMCAT; } 
+        catch (ClassNotFoundException e)    { ret = CODE_TOMCAT; } 
+        catch (InstantiationException e)    { ret = CODE_TOMCAT; }
+        catch (SQLException e)              { ret = CODE_DB; }
+        catch (IOException e)               { ret = CODE_GATEWAY_THREAD; }
+        finally {
+            if (connRegistration != null) {
+                try { connRegistration.close(); System.out.println("connRegistration database connection terminated"); } 
+                catch (Exception e) {}
+            }
+            if (connPresence != null) {
+                try { connPresence.close(); System.out.println("connPresence database connection terminated"); } 
+                catch (Exception e) {}
+            }
         }
 
-        addUser(uni, icalid, icalpass, icalurl, icaluri, icalport, ssl);
+        try {
+            if (ret != CODE_OK)
+            {
+                HttpSession session = request.getSession(true);
+                session.setAttribute("emessage", ERROR_MESSAGES[ret]);
+                response.sendRedirect("error.jsp");
+            }
+            else
+                response.sendRedirect("success.jsp");
+        } catch (IOException e) { throw new ServletException(e); }
+    }
 
-
-        CalendarAccount calAccount = new CalendarAccount(icalid, icalpass.toCharArray(), icalurl, icaluri, Integer.parseInt(icalport), sslVal);
-        GatewayUser newUser = GatewayUser.createUser(uni, calAccount, null);
-        Socket socket = new Socket("127.0.0.1", ServerParameters.REGISTRATION_PORT);
-        //ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());        
-        DataOutputStream oos = new DataOutputStream(socket.getOutputStream());
-        //oos.writeObject(newUser);
-        oos.writeUTF(uni);
-        oos.writeUTF(icalid);
-        oos.writeUTF(icalpass);
-        oos.writeUTF(icalurl);
-        oos.writeUTF(icaluri);
-        oos.writeUTF(icalport);
-        oos.writeUTF(ssl);
-
-        oos.close();
-
-
-
-        response.sendRedirect("success.jsp");
-
-
-
-
-
+    private Connection createDBConn(String userName, String passwd, String url) 
+            throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException 
+    {
+        Connection conn = null;
+        System.out.println("Database connection established");
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        conn = DriverManager.getConnection(url, userName, passwd);
+        return conn;
     }
 
     /** 
@@ -163,58 +166,55 @@ public class addServlet extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
-    public void addUser(String uni, String icalid, String icalpass, String icalurl, String icaluri, String icalport, String ssl) {
+    private void pipeRegistrationToGateway( String uni, 
+                                            String icalid, 
+                                            String icalpass, 
+                                            String icalurl, 
+                                            String icaluri, 
+                                            String icalport, 
+                                            String ssl) throws IOException
+    {   
+        Socket socket = new Socket("127.0.0.1", ServerParameters.REGISTRATION_PORT);
+        DataOutputStream oos = new DataOutputStream(socket.getOutputStream());
 
-        int sslVal;
+        oos.writeUTF(uni);
+        oos.writeUTF(icalid);
+        oos.writeUTF(icalpass);
+        oos.writeUTF(icalurl);
+        oos.writeUTF(icaluri);
+        oos.writeUTF(icalport);
+        oos.writeUTF(ssl);
 
-        if (ssl.equals("Yes")) {
-            sslVal = 1;
-        } else {
-            sslVal = 0;
-        }
+        oos.close();
+        socket.close();
+    }
+    
+    public void addUserRegistrationDB(Connection conn,  String uni, 
+                                                        String icalid, 
+                                                        String icalpass, 
+                                                        String icalurl, 
+                                                        String icaluri, 
+                                                        String icalport, 
+                                                        String ssl) throws SQLException 
+    {
+        int sslVal = ssl.equals("Yes") ? 1 : 0;
 
-        Connection conn = null;
+        Statement s = conn.createStatement();
+        String sqlquery = null;
 
-        try {
+        sqlquery = "INSERT INTO icalgateway.registrations (cuid, ical_userid, ical_passwd, ical_host, ical_uri, ical_port, ical_ssl) VALUES ('" + uni + "','" + icalid + "','" + icalpass + "','" + icalurl + "','" + icaluri + "','" + icalport + "','" + sslVal + "');";
+        s.executeUpdate(sqlquery);
+        s.close();
+    }
+    
+    public void addUserToPresenceDB(Connection conn,String uni, 
+                                                    String icalpass) throws SQLException 
+    {
+        Statement s = conn.createStatement();
+        String sqlquery = null;
 
-            String userName = "btuser";
-            String passwd = "passwd";
-            String url = "jdbc:mysql://localhost:3306/icalgateway";
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            conn = DriverManager.getConnection(url, userName, passwd);
-            System.out.println("Database connection established");
-            Statement s = conn.createStatement();
-            String sqlquery = null;
-
-            sqlquery = "INSERT INTO icalgateway.registrations (cuid, ical_userid, ical_passwd, ical_host, ical_uri, ical_port, ical_ssl) VALUES ('" + uni + "','" + icalid + "','" + icalpass + "','" + icalurl + "','" + icaluri + "','" + icalport + "','" + sslVal + "');";
-            s.executeUpdate(sqlquery);
-
-            s.close();
-            conn.close();
-
-            String url2 = "jdbc:mysql://128.59.18.182:3306/Presence";
-            conn = DriverManager.getConnection(url2, userName, passwd);
-            s = conn.createStatement();
-            sqlquery = "INSERT INTO presence.login(user,password) VALUES ('" + uni + "','" + icalpass + "')";
-            s.executeUpdate(sqlquery);
-            s.close();
-
-
-
-        } catch (Exception e) {
-            System.err.println(e);
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                    System.out.println("Database connection terminated");
-
-                } catch (Exception e) { /* ignore close errors */ }
-            }
-        }
-
-
-
-
+        sqlquery = "INSERT INTO presence.login(user,password) VALUES ('" + uni + "','" + icalpass + "')";
+        s.executeUpdate(sqlquery);
+        s.close();
     }
 }
---
+++ trunk/GatewaySite/web/error.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : error.jsp
+    Created on : Dec 13, 2008, 5:16:03 PM
+    Author     : milind
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+   
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Error</title>
+    </head>
+    <body>
+        <h2>An error occurred</h2>
+        <p><%=session.getAttribute("emessage")%></p>
+    </body>
+</html>
